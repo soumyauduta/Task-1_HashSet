a11b24955d7c1b849d51759a42a5bcf6366ab2d0
soumyauduta <107532156+soumyauduta@users.noreply.github.com>
2022-08-15 11:58:52 +0530

    Add files via upload

---
+++ Hset.java
@@ -0,0 +1,14 @@
+import java.util.HashSet;
+
+public class Hset {
+    public static void main(String[] args) {
+        HashSet<Integer> hs=new HashSet<>();
+        hs.add(10);
+        hs.add(18);
+        hs.add(12);
+        hs.add(2);
+        hs.add(20);
+        System.out.println("Stored Elements: "+hs);
+
+    }
+}
